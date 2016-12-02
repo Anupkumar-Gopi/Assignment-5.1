17df78c29da6c52f84936605d1648ac25f7f6fc7
Anupkumar-Gopi <anupgopi.webdev@gmail.com>
2016-12-02 15:01:13 +0530

    Assignment-5.1

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var name=prompt("Enter your name\neg. 'Anupkumar Gopi'");
+var age=prompt("Enter your age\neg. '29'");
+var dob=prompt("Enter your date of birth\neg. '01/01/1990'");
+var pob=prompt("Enter your place of birth\neg. 'Kerala'");
+console.log("Name : "+name+"\nAge : "+age+"\nDate of Birth : "+dob+"\nPlace of Birth : "+pob);
+
